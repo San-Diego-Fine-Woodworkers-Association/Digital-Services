485b9a8431f6dfdfded4dfce6f87e5438189fcaf
saigeethakandepallicherukuru <saigeethakc@gmail.com>
2026-04-06 16:08:13 -0700

    SDFWA-DB: add more tables

--- apps/shop-ops/lib/db.ts
+++ apps/shop-ops/lib/db.ts
@@ -3,4 +3,11 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./db/schema";
 
 export const db = drizzle(process.env.DATABASE_CONNECTION_STRING!, { schema });
-export { reporterTable } from "./db/schema";
+export {
+	reporterTable,
+	toolTable,
+	maintainerTable,
+	toolMaintainerTable,
+	statusesTable,
+	logEntryTable,
+} from "./db/schema";
